87dff90ccf04fcdcc4f6baa817d827235fd824e7
Stanislav Kindrat <kindratsm@gmail.com>
2019-06-11 19:41:36 +0300

    Implement Swagger support

--- DotNetCoreApi-CodeHubGreece/Startup.cs
+++ DotNetCoreApi-CodeHubGreece/Startup.cs
@@ -1,6 +1,10 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Reflection;
+using System.Text;
+using Microsoft.AspNet.OData;
 using Microsoft.AspNet.OData.Builder;
 using Microsoft.AspNet.OData.Extensions;
 using Microsoft.AspNetCore.Builder;
@@ -8,16 +12,23 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
 using Microsoft.OData.Edm;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace DotNetCoreApi_CodeHubGreece
 {
     public class Startup
     {
+        private readonly String JsonPath;
+
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
+
+            JsonPath = Path.Combine(System.AppContext.BaseDirectory, "json");
+            Directory.CreateDirectory(JsonPath);
         }
 
         public IConfiguration Configuration { get; }
@@ -43,25 +54,45 @@ namespace DotNetCoreApi_CodeHubGreece
             })
                 .ConfigureApplicationPartManager(apm => apm.FeatureProviders.Add(new Helpers.GenericODataFeatureProvider()))
                 .AddJsonFormatters(options => options.ReferenceLoopHandling = ReferenceLoopHandling.Ignore)
+                .AddApiExplorer()
                 .SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            // Register the Swagger generator
+            services.AddSwaggerGen();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            app.UseStaticFiles(new StaticFileOptions
+            {
+                FileProvider = new PhysicalFileProvider(JsonPath),
+                RequestPath = "/json"
+            });
             app.UseHsts();
             app.UseCors((builder) =>
             {
                 builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader();
             });
+
             app.UseMvc((routeBuilder) =>
             {
                 routeBuilder.MapODataServiceRoute("odata", "odata", GetEdmModel());
                 routeBuilder.Select().Expand().Filter().OrderBy().Count();
             });
+
+            // Enable middleware to serve generated Swagger as a JSON endpoint
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.), 
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI((options) =>
+            {
+                options.SwaggerEndpoint("/json/swagger.json", "OData");
+            });
         }
 
-        private static IEdmModel GetEdmModel()
+        private IEdmModel GetEdmModel()
         {
             ODataConventionModelBuilder builder = new ODataConventionModelBuilder();
 
@@ -77,7 +108,36 @@ namespace DotNetCoreApi_CodeHubGreece
                 genericMethod.Invoke(builder, new object[] { candidate.Name });
             }
 
-            return builder.GetEdmModel();
+            var model = builder.GetEdmModel();
+
+            var converter = new ODataSwaggerConverter(model);
+            var swaggerModel = converter.GetSwaggerModel();
+            var swaggerPaths = (JObject)swaggerModel.GetValue("paths");
+
+            List<JProperty> childrenToRemove = new List<JProperty>();
+
+            foreach (var property in swaggerPaths.Properties())
+            {
+                var path = property.Value<JToken>();
+                var obj = (JObject)path.FirstOrDefault();
+                foreach (var child in obj.Children<JProperty>())
+                {
+                    if (child.Name == "patch")
+                    {
+                        childrenToRemove.Add(child);
+                    }
+                }
+            }
+
+            childrenToRemove.ForEach((child) =>
+            {
+                child.Remove();
+            });
+
+            var swaggerPath = Path.Combine(JsonPath, "swagger.json");
+            File.WriteAllText(swaggerPath, converter.GetSwaggerModel().ToString(), Encoding.UTF8);
+
+            return model;
         }
     }
 }
